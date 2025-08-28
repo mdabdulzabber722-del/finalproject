bc920ec2caf9a5e8e3dde67895c421cb33e56dc2
mdabdulzabber722-del <mdabdulzabber722@gmail.com>
2025-08-28 17:33:11 +0600

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
     sourcemap: false,
+    assetsDir: 'assets',
     rollupOptions: {
-      input: './index.html'
+      input: './index.html',
+      output: {
+        manualChunks: undefined,
+      }
     }
   },
   server: {
